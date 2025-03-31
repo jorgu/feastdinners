aba66ebacea58ce8c2448187d9f9b7da19ea0512
jorgu <jorgu@users.noreply.github.com>
2025-03-31 07:29:00 +0200

    Update feast.js

--- feast.js
+++ feast.js
@@ -4,51 +4,47 @@
 https://jorgu/feastdinners.github.io;
 
 */
+let feastHolidayList    = [];
 let upcomingFeastDinner = {name: 'none', feastDate: new Date()};
 let timeStampNow        = new Date();
-let today               = new Date(); 
+let jan = 0, feb=1, mar=2, apr=3, maj=4, jun=5, jul=6, aug=7, sep=8, okt=9, nov=10, dec=11; 
 
 let year			    = timeStampNow.getFullYear();
 let month               = timeStampNow.getMonth();
 let day                 = timeStampNow.getDay();
-const EPIPHANY          = {id: 'epiphany', desc: 'Trettondag'};
-const EASTEREVENING     = {id: 'easter', desc: 'Påskafton'};
-const MIDSUMMEREVENING  = {id: 'midsummer', desc: 'Midsommar'};
-const CRAYFISHPARTY     = {id: 'crayfish', desc: 'Kräftskiva'};
-const CHRISTMASDINNER   = {id: 'christmasdinner', desc: 'Julbord'};
-const CHRISTMASEVENING  = {id: 'christmaseve', desc: 'Julafton'};
-const NEWYEARDINNER     = {id: 'newyear', desc: 'Nyårsafton'};
 
-let jan = 0, feb=1, mar=2, apr=3, maj=4, jun=5, jul=6, aug=7, sep=8, okt=9, nov=10, dec=11; 
-  
-today.setFullYear(year, month, day); // månad: 0=jan, 1=feb, ...
+const EPIPHANY          = {id: 'epiphany',          desc: 'Trettondag', date: new Date(timeStampNow.getFullYear(), jan, 10),   urlID: '',                                              sheetID: ''};
+const EASTEREVENING     = {id: 'easter',            desc: 'Påskafton',  date: new Date(timeStampNow.getFullYear(), apr, 24),   urlID: '1eZv-inNeGf_b0hfx1Gqkr2Vx6e8ERqUpj_4tnfhn2CY',  sheetID: '401376878'};
+const MIDSUMMEREVENING  = {id: 'midsummer',         desc: 'Midsommar',  date: new Date(timeStampNow.getFullYear(), jun, 24),   urlID: '',                                              sheetID: ''};
+const CRAYFISHPARTY     = {id: 'crayfish',          desc: 'Kräftskiva', date: new Date(timeStampNow.getFullYear(), aug, 18),   urlID: '1ANLJuA04XkSKfIGDlmCI9OnS_kJXcw_z8ju5F3oDcDE',  sheetID: '0'};
+const CHRISTMASDINNER   = {id: 'christmasdinner',   desc: 'Julbord',    date: new Date(timeStampNow.getFullYear(), dec, 6),    urlID: '1JFpuigz7dy6gnQgxBjYi7so75Yj_A5jfUY575uQQxdk',  sheetID: '401376878'};
+const CHRISTMASEVENING  = {id: 'christmaseve',      desc: 'Julafton',   date: new Date(timeStampNow.getFullYear(), dec, 24),   urlID: '1JFpuigz7dy6gnQgxBjYi7so75Yj_A5jfUY575uQQxdk',  sheetID: '740624674'};
+const NEWYEARDINNER     = {id: 'newyear',           desc: 'Nyårsafton', date: new Date(timeStampNow.getFullYear(), dec, 31),   urlID: '',                                              sheetID: ''};
 
-upcomingFeastDinner = getUpcomingfeastdinner(today);
+if( EPIPHANY.urlID.length > 0)            {feastHolidayList.push(EPIPHANY)};
+if( EASTEREVENING.urlID.length > 0)       {feastHolidayList.push(EASTEREVENING)};
+if( MIDSUMMEREVENING.urlID.length > 0)    {feastHolidayList.push(MIDSUMMEREVENING)};
+if( CRAYFISHPARTY.urlID.length > 0)       {feastHolidayList.push(CRAYFISHPARTY)};
+if( CHRISTMASDINNER.urlID.length > 0)     {feastHolidayList.push(CHRISTMASDINNER)};
+if( CHRISTMASEVENING.urlID.length > 0)    {feastHolidayList.push(CHRISTMASEVENING)};
+if( NEWYEARDINNER.urlID.length > 0)       {feastHolidayList.push(NEWYEARDINNER)};
 
-byggStatsistikSite('ffff');
+upcomingFeastDinner = getUpcomingfeastdinner(feastHolidayList);
+
+byggStatsistikSite(feastHolidayList, upcomingFeastDinner);
 //siteTitle		= 'Statistik';    
 
-function getUpcomingfeastdinner() {
+function getUpcomingfeastdinner(lista) {
         
-    let today = new Date();
-    let feastHolidayList = [
-        { id: EPIPHANY.id,            desc: EPIPHANY.desc,           date: new Date(today.getFullYear(), jan, 10),      sheetID:'' },
-        { id: EASTEREVENING.id,       desc: EASTEREVENING.desc,      date: new Date(today.getFullYear(), apr, 24),      sheetID:'' },
-        { id: MIDSUMMEREVENING.id,    desc: MIDSUMMEREVENING.desc,   date: new Date(today.getFullYear(), jun, 24),      sheetID:'' },
-        { id: CRAYFISHPARTY.id,       desc: CRAYFISHPARTY.desc,      date: new Date(today.getFullYear(), aug, 18),      sheetID:'' },
-        { id: CHRISTMASDINNER.id,     desc: CHRISTMASDINNER.desc,    date: new Date(today.getFullYear(), dec, 6),       sheetID:'401376878' },
-        { id: CHRISTMASEVENING.id,    desc: CHRISTMASEVENING.desc,   date: new Date(today.getFullYear(), dec, 24),      sheetID:'740624674' },
-        { id: NEWYEARDINNER.id,       desc: NEWYEARDINNER.desc,      date: new Date(today.getFullYear(), dec, 31),      sheetID:'' },
-        { id: EASTEREVENING.id,       desc: EASTEREVENING.desc,      date: new Date(today.getFullYear()+1, jan, 10),    sheetID:'' }
-    ];
+    let today = new Date(); 
     
-    feastHolidayList.sort((a, b) => a.date - b.date);
+    lista.sort((a, b) => a.date - b.date);
 
     // Hitta det första datumet som är större än dagens datum
-    let nextHoliday = feastHolidayList.find(event => event.date > today);
+    let nextHoliday = lista.find(event => event.date > today);
 
-    console.log(`Nästa högtid: ${nextHoliday.desc} - ${nextHoliday.date.toDateString()}`);
-    return nextHoliday;
+    console.log(`Nästa högtid: ${nextHoliday.id} - ${nextHoliday.date.toDateString()}`);
+    return nextHoliday.id;
 }
 
 
@@ -63,15 +59,14 @@ function createElement(element, attribut, attributvarde, text) {
 }
       
 
-function byggStatsistikSite(sheetID) {
+function byggStatsistikSite(lista, holidayDinner) {
   
   
-  //Påskmat.gsheet https://docs.google.com/spreadsheets/d/1eZv-inNeGf_b0hfx1Gqkr2Vx6e8ERqUpj_4tnfhn2CY/edit?gid=401376878#gid=401376878
-
-      var urlTrettondag                = 'window.location.href="https://docs.google.com/spreadsheets/d/' + sheetID + '/htmlembed/sheet?gid=0"';
-      var urlEastern              = 'window.location.href="https://his.se"'; 
-      var urlMidsummer             = 'window.location.href="www.his.se"'; 
-      
+    //Påskmat.gsheet https://docs.google.com/spreadsheets/d/1eZv-inNeGf_b0hfx1Gqkr2Vx6e8ERqUpj_4tnfhn2CY/edit?gid=401376878#gid=401376878
+  
+        let urlPrefix = 'window.location.href="https://docs.google.com/spreadsheets/d/';
+        let urlSuffix = '/htmlembed/sheet?gid=';
+        
       var feastDinnerBody;
       var feastDinnerDivCenter;
       var feastDinnerHeader;
@@ -93,18 +88,15 @@ function byggStatsistikSite(sheetID) {
       feastDinnerHeader.appendChild(feastDinnerDivSub);
       feastDinnerDivCenter.appendChild(feastDinnerHeader);
       
-     /* var feastDinnerBtn1 = createElement('button', 'class', defButtonClass, 'Trettondag');
-          feastDinnerBtn1.setAttribute('onClick', urlTrettondag);
+      
 
-      var feastDinnerBtn2 = createElement('button', 'class', defButtonClass, 'Påsk');
-          feastDinnerBtn2.setAttribute('onClick', urlEastern);
-
-      var feastDinnerBtn3 = createElement('button', 'class', defButtonDiabledClass, 'Midsommar');
-          feastDinnerBtn3.setAttribute('onClick', urlMidsummer);
-          feastDinnerBtn3.setAttribute('disabled',true);
-      */
-      let feastDinnerBtn;
-      feastDinnerBtn = createTheButton(EPIPHANY.desc, urlTrettondag, false, defButtonClass);        
+        lista.forEach(holidy => {
+            let feastDinnerBtn;
+            feastDinnerBtn = createTheButton(holidy.desc, holidy.urlID, holidy.sheetID, holidy.id==holidayDinner, defButtonClass);
+            feastDinnerDivCenter.appendChild(feastDinnerBtn);      
+        })
+    
+    /*  feastDinnerBtn = createTheButton(EPIPHANY.desc, urlTrettondag, false, defButtonClass);        
       feastDinnerDivCenter.appendChild(feastDinnerBtn);
       feastDinnerBtn = createTheButton(EASTEREVENING.desc, urlEastern, false, defButtonClass);
       feastDinnerDivCenter.appendChild(feastDinnerBtn); 
@@ -112,15 +104,15 @@ function byggStatsistikSite(sheetID) {
       feastDinnerDivCenter.appendChild(feastDinnerBtn); 
       feastDinnerBtn = createTheButton(CHRISTMASDINNER.desc, CHRISTMASDINNER.sheetID, false, defButtonClass);
       feastDinnerDivCenter.appendChild(feastDinnerBtn); 
-      
+      */
       feastDinnerBody.appendChild(feastDinnerDivCenter);
       
     }
 
-function createTheButton(description, sheetID, disabled, buttonclass) {
+function createTheButton(description, prefixUrl, sheetID, disabled, buttonclass) {
 
-    let url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSNEXmdDHBEig5YDcvjpi2qhsG583EaivG6s-rHYuOYrYAFYF6MuPTU5sOaMbKynzzTqTmFqnp3vyJd/pubhtml?gid=' + sheetID + '&single=true'
-    url     = 'window.location.href="' +url + '"' 
+    let url = 'window.location.href="https://docs.google.com/spreadsheets/d/' + prefixUrl + '/pubhtml?gid=' + sheetID + '&single=true'
+    
     let feastDinnerBtn = createElement('button', 'class', buttonclass, description);
     feastDinnerBtn.setAttribute('onClick', url);
     feastDinnerBtn.disabled = disabled;
